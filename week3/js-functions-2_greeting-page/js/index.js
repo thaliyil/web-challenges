f7026752db7a1bce40dfdbd4fd762993e8fb7650
Fathimath <faseela863gqqQq@@gmail.com>
2024-10-21 21:57:00 +0200

    write function to display greeting according to time

--- week3/js-functions-2_greeting-page/js/index.js
+++ week3/js-functions-2_greeting-page/js/index.js
@@ -1,25 +1,5 @@
 console.clear();
 
-/*
-Change the contents of this page depending on the current day and time.
-
-- Write a function getGreeting that returns a different greeting depending on the current time:
-  - 6 - 12: returns "Good Morning"
-  - 13 - 18: returns "Good Afternoon"
-  - 19 - 22: returns "Good Evening"
-  - 23 - 5: returns "Good Night"
-
-(HINT: you can get the current time: new Date().getHours() )
-
-- Write a function getDayColor that returns a different color depending on the current weekday:
-  - monday: "darkgray"
-  - tuesday - friday: "lightblue"
-  - saturday - sunday: "hotpink"
-
-(HINT: you can get the current weekday: new Date().getDay() )
-
-*/
-
 const display = document.querySelector('[data-js="display"]');
 const time = new Date().getHours();
 function getGreeting() {
@@ -42,7 +22,6 @@ function getDayColor() {
   } else if (day === "saturday" || "sunday") {
     return "hotpink";
   }
-  // Code here
 }
 
 display.textContent = getGreeting();
