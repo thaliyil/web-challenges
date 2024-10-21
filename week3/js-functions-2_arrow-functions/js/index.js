56098be60e0a9aaba1a0217cf5a5ef7dadd0071c
Fathimath <faseela863gqqQq@@gmail.com>
2024-10-21 21:53:01 +0200

    manipulate DOM using js funcions

--- week3/js-functions-2_arrow-functions/js/index.js
+++ week3/js-functions-2_arrow-functions/js/index.js
@@ -1,22 +1,8 @@
 console.clear();
 
-/*
-Rewrite the following functions as arrow functions.
-Use implicit returns when possible
-*/
 const now = new Date();
 const currentHour = now.getHours();
-/*function getCurrentHour() {
-  if (currentHour === 0) {
-    return "12am";
-  } else if (currentHour === 12) {
-    return "12pm";
-  } else if (currentHour <= 11) {
-    return currentHour + "am";
-  } else {
-    return currentHour - 12 + "pm";
-  }
-}*/
+
 let getCurrenHour = () => {
   if (currentHour === 0) {
     return "12am";
@@ -31,20 +17,11 @@ let getCurrenHour = () => {
 const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 };
-/*function getVectorLength(x, y, z) {
-  return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}*/
-
-/*function cleanInput(string) {
-  return string.toLowerCase().trim();
-}*/
 
 const cleanInput = (string) => {
   return string.toLowerCase().trim();
 };
-/*
-Rewrite the following arrow functions as classic functions.
-*/
+
 function isOddAndSmall(number) {
   if (number > 10) {
     return false;
---
+++ week3/js-functions_dom-manipulation/js/index.js
@@ -0,0 +1,39 @@
+const box = document.querySelector("[data-js=box]");
+const redButton = document.querySelector("[data-js=js-red-button]");
+const greenButton = document.querySelector("[data-js=js-green-button]");
+const blueButton = document.querySelector("[data-js=js-blue-button]");
+const grayButton = document.querySelector("[data-js=js-gray-button]");
+
+
+function removeAllColors() {
+  box.classList.remove("red");
+  box.classList.remove("blue");
+  box.classList.remove("green");
+  box.classList.remove("gray");
+}
+function addColor(color) {
+  box.classList.add(color);
+}
+
+redButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("red");
+});
+
+blueButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("blue");
+});
+
+greenButton.addEventListener("click", () => {
+  removeAllColors();
+
+  addColor("green");
+});
+
+grayButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("gray");
+});
+
+// Write your two functions below:
