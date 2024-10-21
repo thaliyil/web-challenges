c4eb86f65cbe18fd3447d1deef1a83cd7c778b87
Fathimath <faseela863gqqQq@@gmail.com>
2024-10-21 21:38:25 +0200

    create social media page

---
+++ week3/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+const title = "Updates";
+const text = "Showing popularity";
+let numberOfLikes = 320;
+console.log(numberOfLikes);
+numberOfLikes++;
+const user = "";
+const isReported = true;
+
+console.log(user);
+console.log(isReported);
+console.log(numberOfLikes);
